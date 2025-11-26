26e70b5501ab743933e55e63805652be5c04c799
Der-Juli <julian.herrmann94@pm.me>
2025-11-26 20:08:23 +0100

    Update run-lumo-viewer.sh

--- run-lumo-viewer.sh
+++ run-lumo-viewer.sh
@@ -3,14 +3,7 @@
 # Wrapper für Lumo‑Viewer – aktiviert das venv und startet das Skript
 # ---------------------------------------------------------
 
-# Pfad zum Projekt (anpassen, falls anders)
-PROJECT_DIR="/home/juli/projects/priv/Lumo-Viewer"
-
-# in das Projektverzeichnis wechseln
-cd "$PROJECT_DIR" || exit 1
-
 # virtuelles Environment aktivieren
 source ".venv/bin/activate"
-
-# Python‑Skript ausführen (falls es nicht ausführbar ist, benutzen Sie python3)
-exec ./main.py   # <-- "./main.py" muss ausführbar sein (chmod +x)
+# Start Viewer
+exec ./main.py
