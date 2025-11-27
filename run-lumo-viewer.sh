98c5292d5ffb73178baf036589fa427698cac0b9
Der-Juli <julian.herrmann94@pm.me>
2025-11-27 08:33:12 +0100

    Update run-lumo-viewer.sh

--- run-lumo-viewer.sh
+++ run-lumo-viewer.sh
@@ -3,6 +3,9 @@
 # Wrapper für Lumo‑Viewer – aktiviert das venv und startet das Skript
 # ---------------------------------------------------------
 
+# got to working dir
+cd 
+
 # virtuelles Environment aktivieren
 source ".venv/bin/activate"
 # Start Viewer
